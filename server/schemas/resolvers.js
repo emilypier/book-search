d9a694ec1d0936d2a5d69506206a9b55ef8c0bc2
emilypier <piersoee@miamioh.edu>
2022-04-01 16:06:05 -0400

    made typeDefs and resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,68 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parents, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).select(
+          "-__v -password"
+        );
+
+        return user;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(context.user._id, {
+          $push: { saveBooks: bookData },
+        });
+
+        return user;
+      }
+      throw new AuthenticationError("Incorrect credentials");
+    },
+
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const user = await User.findByIdAndUpdate(context.user._id, {
+          $pull: { saveBooks: { bookId } },
+        });
+
+        return user;
+      }
+      throw new AuthenticationError("Incorrect credentials");
+    },
+  },
+};
+
+module.exports = resolvers;
