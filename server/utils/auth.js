ab0a0d919b6f1768684a7cb82032bdfe7fdea7eb
emilypier <piersoee@miamioh.edu>
2022-04-03 17:08:47 -0400

    updated auth.js + server/server.js to test w graphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -26,19 +26,12 @@ module.exports = {
     } catch {
       console.log('Invalid token');
     }
-
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
-
+    
     return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
